edb1cac7c3600d6fbf1df1a7f2dfc6d105d72888
Pochang Lee <pochanglee2008@gmail.com>
2023-10-06 22:49:53 +0800

    add content script

---
+++ content.tsx
@@ -0,0 +1,31 @@
+import type { PlasmoCSConfig } from 'plasmo'
+
+import { highlight } from '~utils/highlight'
+import { findSite } from '~utils/site'
+import type { ISite } from '~utils/site'
+
+export const config: PlasmoCSConfig = {
+  matches: ['<all_urls>'],
+  run_at: 'document_idle'
+}
+
+const sites: ISite[] = [
+  {
+    uri_pattern: 'https://docs.plasmo.com',
+    rules: [
+      {
+        pattern: 'Welcome',
+        color: 'FF0000'
+      }
+    ]
+  }
+]
+
+const site = findSite(window.location.href, sites)
+
+if (site !== null) {
+  document.body.innerHTML = highlight({
+    html: document.body.innerHTML,
+    rules: site.rules
+  })
+}
