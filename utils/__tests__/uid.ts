949a39a36265b0e5de665ce556831277afcf3f3a
Pochang Lee <pochanglee2008@gmail.com>
2023-10-13 23:06:48 +0800

    fix: getUid collision

--- utils/__tests__/uid.ts
+++ utils/__tests__/uid.ts
@@ -4,39 +4,53 @@ import { getUid, toBase64 } from '~utils/uid'
 
 describe('toBase64', () => {
   test('0', () => {
-    expect(toBase64(0)).toEqual('A')
+    expect(toBase64(0n)).toEqual('A')
   })
   test('1', () => {
-    expect(toBase64(1)).toEqual('B')
+    expect(toBase64(1n)).toEqual('B')
   })
   test('63', () => {
-    expect(toBase64(63)).toEqual('_')
+    expect(toBase64(63n)).toEqual('_')
   })
   test('64', () => {
-    expect(toBase64(64)).toEqual('BA')
+    expect(toBase64(64n)).toEqual('BA')
   })
   test('65', () => {
-    expect(toBase64(65)).toEqual('BB')
+    expect(toBase64(65n)).toEqual('BB')
   })
   test('1697198134084000', () => {
-    expect(toBase64(1697198134084000)).toEqual('GB5e75UGg')
+    expect(toBase64(1697198134084000n)).toEqual('GB5e75UGg')
   })
 })
 
 describe('getUid', () => {
   test('version increment', () => {
-    const version1 = getUid() % 100
-    const version2 = getUid() % 100
-    expect(version2).toEqual(version1 + 1)
+    const version1 = getUid() % 10000n
+    const version2 = getUid() % 10000n
+    expect(version2).toEqual(version1 + 1n)
   })
   test('precision', () => {
     // if integer is too big, like (new Date().getTime() * 10000, it loses its precision
-    let prev: number
-    let current = getUid() % 100
+    let prev: bigint
+    let current = getUid() % 1000n
     do {
       prev = current
-      current = getUid() % 100
-      expect(prev).toEqual((current + 99) % 100)
-    } while (current != 0)
+      current = getUid() % 1000n
+      expect(prev).toEqual((current + 999n) % 1000n)
+    } while (current != 0n)
+  })
+  test('collision', () => {
+    let length = 1000000
+    const uids = new Array(length).fill(0).map(getUid)
+    const set = new Set(uids)
+    expect(uids.length).toEqual(length)
+
+    expect(Array.from(set).length).toEqual(length)
+  })
+  test('makes sure milliseconds ticks at least twice for every 100000 uid', () => {
+    let buckets = 100000
+    const uids = new Array(buckets * 10).fill(0).map(getUid)
+    const diff = Number(uids[uids.length - 1] - uids[0])
+    expect(diff).toBeGreaterThan(buckets * 30)
   })
 })
--- utils/uid.ts
+++ utils/uid.ts
@@ -1,26 +1,29 @@
-let version = 0
+let version = 0n
 const BASE64_LETTERS =
   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_'
 
-export function toBase64(num: number): string {
+export function toBase64(num: bigint): string {
   const remainders: number[] = []
-  let remainder: number
+  let remainder: bigint
   do {
-    remainder = num % 64
-    remainders.push(remainder)
-    num = Math.floor(num / 64)
+    remainder = num % 64n
+    remainders.push(Number(remainder))
+    num = num / 64n
   } while (num >= 1)
   remainders.reverse()
   return remainders.map((index) => BASE64_LETTERS.at(index)).join('')
 }
 
-export function getUid(): number {
-  version = version + 1
-  const timestamp = new Date().getTime()
-  const uid = timestamp * 100 + version
+export function getUid(): bigint {
+  const buckets = 100000n
+
+  version = version + 1n
+  const timestamp = BigInt(new Date().getTime())
+  const uid = timestamp * buckets + (version % buckets)
   return uid
 }
 
 export function getUid64(): string {
+  // a method to get unique uid in single thread environment
   return toBase64(getUid())
 }
